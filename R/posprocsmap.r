f30998d48babb93b9c45da38c068a32f2f85184f
Lucas de Souza Khenayfis <lucaskhenayfis@gmail.com>
2023-05-30 14:19:19 -0300

    fix -2

---
+++ R/posprocsmap.r
@@ -0,0 +1,8 @@
+#' posproc-smap
+#' 
+#' Projeto para desenvolvimento de metodologias para pos processamento das previsoes do SMAP/ONS
+
+library(languageserver)
+library(dbrenovaveis)
+library(arrow)
+library(aws.s3)
