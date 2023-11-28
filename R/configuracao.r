77958b063c0415f2c6295aa3b6a3110ac083765f
Lucas de Souza Khenayfis <lucaskhenayfis@gmail.com>
2023-11-28 17:46:59 -0300

    Adapta leitura de config p transformacoes

--- R/configuracao.r
+++ R/configuracao.r
@@ -56,11 +56,6 @@ le_conf_posproc_diario <- function(arq_conf, log = FALSE, print = FALSE, early =
 
     janela_i <- as.Date(janela[[1]][1])
 
-    janela_max_i <- janela_i - (max_janela + max_hor)
-    janela_max_f <- as.Date(janela[[2]][2]) - 1
-
-    CONF$PARAMETROS$janela_dados <- paste0(janela_max_i, "/", janela_max_f)
-
     CONF$PARAMETROS$janela_hormod <- lapply(all_hors, function(h) {
         lapply(CONF$MODELOS, function(m) {
             jj <- tail(m$janela, 1)
@@ -70,6 +65,26 @@ le_conf_posproc_diario <- function(arq_conf, log = FALSE, print = FALSE, early =
             paste0(janela_i, "/", janela_f)
         })
     })
+
+    # TRANSFORMACAO DE DADOS ---------------------------------------------
+
+    janela_transf <- CONF$TRANSFORMACAO$janela
+    if (is.null(janela_transf)) janela_transf <- CONF$PARAMETROS$janela
+    janela_transf <- dbrenovaveis:::parsedatas(janela_transf, "", FALSE)
+    janela_transf <- c(as.Date(janela_transf[[1]][1]), as.Date(janela_transf[[2]][2]) - 1)
+
+    janela_max_i <- min(janela_i - (max_janela + max_hor), janela_transf[1])
+    janela_max_f <- as.Date(janela[[2]][2]) - 1
+
+    subset <- seq.Date(janela_max_i, janela_max_f, "days") <= janela_transf[2]
+
+    transf_call <- as.symbol(paste0("transforma_", CONF$TRANSFORMACAO$tipo))
+    transf_call <- as.call(c(list(transf_call), CONF$TRANSFORMACAO$parametros))
+    transf_call$subset <- subset
+
+    CONF$TRANSFORMACAO <- list(call = transf_call, janela = janela_transf)
+
+    CONF$PARAMETROS$janela_dados <- paste0(janela_max_i, "/", janela_max_f)
 
     # MONTA CALLS DOS MODELOS ---------------------------------------
 
