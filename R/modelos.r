1b57093da21b5f3503519a2927a9e52255db8ac2
Lucas de Souza Khenayfis <lucaskhenayfis@gmail.com>
2023-12-05 14:09:01 -0300

    Doc melhor as funs em modelos.r

--- R/modelos.r
+++ R/modelos.r
@@ -9,6 +9,50 @@
 #' Estas funcoes sao necessarias para aplicacao de quaisquer transformacoes necessarias ao dado
 #' antes da execucao das funcoes do modprev. No caso mais simples, sao um wrapper dummy que 
 #' simplesmente chama janelamovel
+#' 
+#' Qualquer funcao declarada aqui deve seguir alguns padroes de implementacao. Todas precisam 
+#' receber quatro argumentos nomeados: "erros", "vazoes", "previstos" e "assimilados", 
+#' correspondendo a dados que sao passados automaticamente por dentro da execucao principal. A 
+#' seguir sao descritos os formatos de cada um destes dados
+#' 
+#' * erros
+#' 
+#'        data | data_execucao | dia_previsao |        erro
+#'   ---       |    ---        |     ---      |     ---
+#'  2013-01-01 |    2012-12-31 |            1 |   -8.882009
+#'  2013-01-02 |    2013-01-01 |            1 |  -10.123693
+#'  2013-01-03 |    2013-01-02 |            1 |  -11.997165
+#'  2013-01-04 |    2013-01-03 |            1 |  -11.694910
+#'  2013-01-05 |    2013-01-04 |            1 |  -11.177255
+#' 
+#' Quando pos processando, por exemplo, cinco dias a frente, este dado contera os erros nas datas
+#' a serem corrigidas em todos os horizontes de previzao até cinco
+#' 
+#' * previstos 
+#' 
+#'  data_previsao | data_execucao | dia_previsao | precipitacao |  vazao
+#'     ---        |       ---     |     ---      |     ---      |   ---
+#'  2012-12-28    | 2012-12-27    |        1     |     0.02     |  116.41
+#'  2012-12-28    | 2012-12-26    |        2     |     0.02     |  118.42
+#'  2012-12-28    | 2012-12-25    |        3     |     0.02     |  123.74
+#'  2012-12-28    | 2012-12-24    |        4     |     0.02     |  125.21
+#'  2012-12-28    | 2012-12-23    |        5     |     0.02     |  123.81
+#' 
+#' Mesmo esquema de dados, porem contendo H - 1 datas a mais, ateriores ao inicio de 'erros'. Desta
+#' forma, 'previstos' pode conter todos os trajetos de previsao H passos a frente ate cada uma das
+#' datas em 'erros' a serem pos processadas
+#' 
+#' * assimilados
+#' 
+#'  data       |  dia_assimilacao |    rsolo  |     rsub  |    rsup     | rsup2
+#'     ---     |      ---         |    ---    |    ---    |    ---      |  ----
+#'  2012-12-27 |         1        | 21.295739 | 173.71275 | 0.009991589 | 0
+#'  2012-12-28 |         1        | 22.197630 | 174.55094 | 0.015860659 | 0
+#'  2012-12-29 |         1        | 22.928107 | 175.39317 | 0.025177226 | 0
+#'  2012-12-30 |         1        | 23.687665 | 176.23946 | 0.039966355 | 0
+#'  2012-12-31 |         1        | 24.456952 | 177.08984 | 0.063442634 | 0
+#' 
+#' As datas em 'assimilados' correspondem as DATAS DE EXECUCAO EM PREVISTOS.
 
 ####################################################################################################
 
